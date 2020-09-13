e0e5a1d39ee93fc32791db67b3d5ed746765bf61
Rohit Chaudhary <40576340+rorochaudhary@users.noreply.github.com>
2020-09-13 12:01:23 -0700

    potential async solution

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var express = require('express');
 var request = require('request');
 var request_promise = require('request-promise');
 var states = require('./public/states.json');
-var api_keys = require('./api-keys.js');
+// var api_keys = require('./api-keys.js');
 var mapquest_base_url = 'http://open.mapquestapi.com/geocoding/v1/address';
 
 var app = express();
@@ -85,7 +85,7 @@ function validateLocation(user_input) {
 }
 
 // request the COVID API using validated, user-inputted city/state
-function covidReqHandler(county_state) {
+function covidReqHandler(county_state, callback){
     // generate the api request url based on county name
     let covidAPI = "https://www.trackcorona.live/api/cities/"
     let county_substrings = county_state.county.split(" ");
@@ -96,14 +96,9 @@ function covidReqHandler(county_state) {
     covidAPI = covidAPI.slice(0, -3);        // remove the last %20
 
     // send the request
-    let options = {
-        method: "GET",
-        uri: covidAPI
-    }
-
-    request_promise(options)
-        .then(function (response) {
-            let info = JSON.parse(response);
+    request(covidAPI, function(err, response, body) {
+        if (!err && response.statusCode < 400) {
+            let info = JSON.parse(response.body);
             let results = info.data;
             let covidData;
 
@@ -126,22 +121,123 @@ function covidReqHandler(county_state) {
                     }
                 }
                 console.log("found county COVID data:", covidData);
-                return covidData;
+                // return covidData;
+                callback(covidData);
             }
-        })
-        .catch(function (err) {
-            console.log(err);
+        } else {
             if (response) {
                 console.log(response.statusCode);
             }
-            return false;
-        });
+            next(err);
+        }
+    });
 }
+
+// const getCovidData = async (county_state) => {
+//     // generate the api request url based on county name
+//     let covidAPI = "https://www.trackcorona.live/api/cities/"
+//     let county_substrings = county_state.county.split(" ");
+//     for (let i=0; i < county_substrings.length; i++) {
+//         covidAPI += county_substrings[i];
+//         covidAPI += "%20";
+//     }
+//     covidAPI = covidAPI.slice(0, -3);        // remove the last %20
+
+//     const result = await covidRequest(covidAPI, county_state);
+//     return result;
+// }
+
+// const covidRequest = async (apiAddr, county_state) => {
+//     // send the request
+//     request(apiAddr, function(err, response, body) {
+//         if (!err && response.statusCode < 400) {
+//             let info = JSON.parse(response.body);
+//             let results = info.data;
+//             let covidData;
+
+//             if (results.length == 0) {
+//                 return false;
+//             } else if (results.length == 1){
+//                 covidData = results[0];
+//                 return covidData;
+//             } else {        // multiple county results, need to get the correct state
+//                 let state_to_find;
+//                 for (let j=0; j < states.length; j++) {
+//                     if (states[j].Code == county_state.state) {
+//                         state_to_find = states[j].State;
+//                     }
+//                 }
+//                 for (let i=0; i < results.length; i++) {
+//                     let state_cmp = results[i].location.split(",");
+//                     if (state_cmp[1].slice(1) == state_to_find) {
+//                         covidData = results[i];
+//                     }
+//                 }
+//                 console.log("found county COVID data:", covidData);
+//                 return covidData;
+//             }
+//         } else {
+//             if (response) {
+//                 console.log(response.statusCode);
+//             }
+//             // next(err);
+//         }
+//     });
+//     return response;
+// }
+
+    // // send the request
+    // let options = {
+    //     method: "GET",
+    //     uri: covidAPI
+    // }
+
+    // request_promise(options)
+    //     .then(function (response) {
+    //         let info = JSON.parse(response);
+    //         let results = info.data;
+    //         let covidData;
+
+    //         if (results.length == 0) {
+    //             return false;
+    //         } else if (results.length == 1){
+    //             covidData = results[0];
+    //             return covidData;
+    //         } else {        // multiple county results, need to get the correct state
+    //             let state_to_find;
+    //             for (let j=0; j < states.length; j++) {
+    //                 if (states[j].Code == county_state.state) {
+    //                     state_to_find = states[j].State;
+    //                 }
+    //             }
+    //             for (let i=0; i < results.length; i++) {
+    //                 let state_cmp = results[i].location.split(",");
+    //                 if (state_cmp[1].slice(1) == state_to_find) {
+    //                     covidData = results[i];
+    //                 }
+    //             }
+    //             console.log("found county COVID data:", covidData);
+    //             return covidData;
+    //         }
+    //     })
+    //     .catch(function (err) {
+    //         console.log(err);
+    //         if (response) {
+    //             console.log(response.statusCode);
+    //         }
+    //         return false;
+    //     });
+
+
 
 // test covidReqHandler and validate location, REMOVE AFTER COMPLETING
 // let test_county_state = {county: "Orange", state: "CA"};
 // let some_data = covidReqHandler(test_county_state);
 // console.log("result: ", some_data);
+// (async () => {
+//     // let data = await getCovidData(test_county_state);
+//     console.log("got data back from API:", await getCovidData(test_county_state));
+// })();
 
 app.get("/", function(req, res){
     var context = {};
@@ -154,15 +250,30 @@ app.get("/main-input-handler", function(req, res) {
     console.log("The user entered:", req.query.location);
 
     // Find the county corresponding to the city and state
-    let county_state_coords = validateLocation(req.query.location);
-    console.log("in main handler, county_state_coords is:", county_state_coords);
+    // let county_state_coords = validateLocation(req.query.location);
+    // console.log("in main handler, county_state_coords is:", county_state_coords);
+    
+    // testing
+    let county_state_coords = {county: "Orange", state: "CA"};
+    // let some_data = covidReqHandler(test_county_state);
+    // console.log("result: ", some_data);     // testing, remove when done
+    
     if (county_state_coords) {
         // get the COVID data for that county
-        let covid_data = covidReqHandler(county_state_coords);
-        if (covid_data) {
-            res.render("results", covid_data);
-        }
-    } else {
+        covidReqHandler(county_state_coords, function(data){
+            console.log("covid_data:", data);
+            res.render("results", data);
+        });
+    }
+
+    // if (county_state_coords) {
+    //     // get the COVID data for that county
+    //     // let covid_data = covidReqHandler(county_state_coords);
+    //     // if (covid_data) {
+    //     //     res.render("results", covid_data);
+    //     // }
+    // } 
+    else {
         res.render("no-results");
     }
 });
