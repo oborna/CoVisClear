3e24335fad1798ce587483521b7d40bc50ecfc56
Rohit Chaudhary <40576340+rorochaudhary@users.noreply.github.com>
2020-09-15 17:45:13 -0700

    404/500 handling, display more covid data, display recovered if given

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ var express = require('express');
 var request = require('request');
 var request_promise = require('request-promise');
 var states = require('./public/states.json');
-// var api_keys = require('./api-keys.js');
-var api_keys = process.env.MAPQUEST_API_KEY;
+var api_keys = require('./api-keys.js');
+// var api_keys = process.env.MAPQUEST_API_KEY;
 var mapquest_base_url = 'http://open.mapquestapi.com/geocoding/v1/address';
 
 require('dotenv').config();
@@ -28,7 +28,7 @@ app.use(express.json());
 // validation and request functions that will be used by routes
 
 // Validate the city/state input by checking data returned from the MapQuest API
-function validateLocation(user_input, get_covid_data) {
+function validateLocation(user_input, get_covid_data, res) {
 
     // Object to pass to the get_covid_data() callback
     let county_state_coords = {
@@ -47,7 +47,7 @@ function validateLocation(user_input, get_covid_data) {
     let longitude = "";
 
     // Build the URL for the call to the API
-    let mapquest_url = mapquest_base_url + `?key=${api_keys}&location=${city_name},${state_name}`;
+    let mapquest_url = mapquest_base_url + `?key=${api_keys.mapquestKey}&location=${city_name},${state_name}`;
     let options = {
         method: "GET",
         uri: mapquest_url
@@ -57,19 +57,20 @@ function validateLocation(user_input, get_covid_data) {
     request_promise(options)
         .then(function (response) {
             let mapquest_data = JSON.parse(response);
+            console.log("mapquest_data:", mapquest_data.results[0]);
             if (mapquest_data && mapquest_data["results"]) {
                 let locations = mapquest_data["results"][0]["locations"];
                 // If the county info doesn't exist, then the city/state pair is invalid
                 if (locations[0]["adminArea4"] === "") {
                     county_state_coords = {};
-                    get_covid_data(county_state_coords);
+                    get_covid_data(county_state_coords, res);
                 }
                 county_name = locations[0]["adminArea4"];
                 latitude = locations[0]["displayLatLng"]["lat"];
                 longitude = locations[0]["displayLatLng"]["lng"];
             } else {
                 county_state_coords = {};
-                get_covid_data(county_state_coords);
+                get_covid_data(county_state_coords, res);
             } 
             county_state_coords["county"] = county_name;
             county_state_coords["state"] = state_name;
@@ -77,169 +78,115 @@ function validateLocation(user_input, get_covid_data) {
             county_state_coords["longitude"] = longitude;
             
             console.log("county_state_coords in validateLocation():", county_state_coords);
-            get_covid_data(county_state_coords);
+            get_covid_data(county_state_coords, res);
         })
         .catch(function (err) {
             console.error(err);
             county_state_coords = {};
-            get_covid_data(county_state_coords);
+            get_covid_data(county_state_coords, res);
         });
 }
 
 // request the COVID API using validated, user-inputted city/state
 function covidReqHandler(county_state, callback){
-    // generate the api request url based on county name
-    let covidAPI = "https://www.trackcorona.live/api/cities/"
-    let county_substrings = county_state.county.split(" ");
-    for (let i=0; i < county_substrings.length; i++) {
-        covidAPI += county_substrings[i];
-        covidAPI += "%20";
-    }
-    covidAPI = covidAPI.slice(0, -3);        // remove the last %20
+    // results passed through covidData
+    let covidData;
 
-    // send the request
-    request(covidAPI, function(err, response, body) {
-        if (!err && response.statusCode < 400) {
-            let info = JSON.parse(response.body);
-            let results = info.data;
-            let covidData;
+    // check if a county_state was found
+    console.log("County results from validate_location():", county_state);
+    if (Object.keys(county_state).length == 0) {
+        console.log("no county found");
+        covidData = false;
+        callback(covidData);
+    } else {
+        // generate the api request url based on county name
+        let covidAPI = "https://www.trackcorona.live/api/cities/"
+        let county_substrings = county_state.county.split(" ");
+        for (let i=0; i < county_substrings.length; i++) {
+            covidAPI += county_substrings[i];
+            covidAPI += "%20";
+        }
+        covidAPI = covidAPI.slice(0, -3);        // remove the last %20
 
-            if (results.length == 0) {
-                return false;
-            } else if (results.length == 1){
-                covidData = results[0];
-                return covidData;
-            } else {        // multiple county results, need to get the correct state
-                let state_to_find;
-                for (let j=0; j < states.length; j++) {
-                    if (states[j].Code == county_state.state) {
-                        state_to_find = states[j].State;
+        // send the request
+        request(covidAPI, function(err, response, body) {
+            if (!err && response.statusCode < 400) {
+                let info = JSON.parse(response.body);
+                let results = info.data;
+                // console.log("results from request: ", results);
+
+                // deal with none, single, multiple COVID API responses
+                if (results.length == 0) {
+                    covidData = false;
+                    callback(covidData);
+                } else if (results.length == 1) {
+                    covidData = results[0];
+                    console.log("found county COVID data (from single result):", covidData);
+                    callback(covidData);
+                } else {        // multiple county results, need to get the correct state
+                    let state_to_find;
+                    for (let j=0; j < states.length; j++) {
+                        if (states[j].Code == county_state.state) {
+                            state_to_find = states[j].State;
+                        }
                     }
-                }
-                for (let i=0; i < results.length; i++) {
-                    let state_cmp = results[i].location.split(",");
-                    if (state_cmp[1].slice(1) == state_to_find) {
-                        covidData = results[i];
+                    for (let i=0; i < results.length; i++) {
+                        let state_cmp = results[i].location.split(",");
+                        if (state_cmp[1].slice(1) == state_to_find) {
+                            covidData = results[i];
+                        }
                     }
+                    console.log("found county COVID data (from multiple results):", covidData);
+                    callback(covidData);
                 }
-                console.log("found county COVID data:", covidData);
-                // return covidData;
+            } else {
+                if (response) {
+                    console.log(response.statusCode);
+                }
+                covidData = false;
                 callback(covidData);
             }
-        } else {
-            if (response) {
-                console.log(response.statusCode);
-            }
-            next(err);
-        }
-    });
+        });
+    } 
 }
 
-// const getCovidData = async (county_state) => {
-//     // generate the api request url based on county name
-//     let covidAPI = "https://www.trackcorona.live/api/cities/"
-//     let county_substrings = county_state.county.split(" ");
-//     for (let i=0; i < county_substrings.length; i++) {
-//         covidAPI += county_substrings[i];
-//         covidAPI += "%20";
-//     }
-//     covidAPI = covidAPI.slice(0, -3);        // remove the last %20
+// called by GET route handler in order to het
+function retrieve_covid_data(county_state_coords, res) {
+    console.log("county_state_coords in retrieve_covid_data():", county_state_coords);
 
-//     const result = await covidRequest(covidAPI, county_state);
-//     return result;
-// }
+    // check for no county found
+    if (county_state_coords == {}) {
+        console.log("No county found.");
+        res.render('no-results');
+    } else if (county_state_coords["county"] !== "") {
+        // get the COVID data for that county
+        console.log("finding COVID data for... ", county_state_coords["county"]);
+        covidReqHandler(county_state_coords, function(data){
+            if (data != false) {
+                console.log("covid_data:", data);
 
-// const covidRequest = async (apiAddr, county_state) => {
-//     // send the request
-//     request(apiAddr, function(err, response, body) {
-//         if (!err && response.statusCode < 400) {
-//             let info = JSON.parse(response.body);
-//             let results = info.data;
-//             let covidData;
+                // clean up date value
+                last_updated = data.updated.split(" ");
+                console.log(last_updated);
 
-//             if (results.length == 0) {
-//                 return false;
-//             } else if (results.length == 1){
-//                 covidData = results[0];
-//                 return covidData;
-//             } else {        // multiple county results, need to get the correct state
-//                 let state_to_find;
-//                 for (let j=0; j < states.length; j++) {
-//                     if (states[j].Code == county_state.state) {
-//                         state_to_find = states[j].State;
-//                     }
-//                 }
-//                 for (let i=0; i < results.length; i++) {
-//                     let state_cmp = results[i].location.split(",");
-//                     if (state_cmp[1].slice(1) == state_to_find) {
-//                         covidData = results[i];
-//                     }
-//                 }
-//                 console.log("found county COVID data:", covidData);
-//                 return covidData;
-//             }
-//         } else {
-//             if (response) {
-//                 console.log(response.statusCode);
-//             }
-//             // next(err);
-//         }
-//     });
-//     return response;
-// }
+                data.updated = {
+                    date: last_updated[0], 
+                    time: last_updated[1].split(".")[0]
+                }
 
-    // // send the request
-    // let options = {
-    //     method: "GET",
-    //     uri: covidAPI
-    // }
-
-    // request_promise(options)
-    //     .then(function (response) {
-    //         let info = JSON.parse(response);
-    //         let results = info.data;
-    //         let covidData;
-
-    //         if (results.length == 0) {
-    //             return false;
-    //         } else if (results.length == 1){
-    //             covidData = results[0];
-    //             return covidData;
-    //         } else {        // multiple county results, need to get the correct state
-    //             let state_to_find;
-    //             for (let j=0; j < states.length; j++) {
-    //                 if (states[j].Code == county_state.state) {
-    //                     state_to_find = states[j].State;
-    //                 }
-    //             }
-    //             for (let i=0; i < results.length; i++) {
-    //                 let state_cmp = results[i].location.split(",");
-    //                 if (state_cmp[1].slice(1) == state_to_find) {
-    //                     covidData = results[i];
-    //                 }
-    //             }
-    //             console.log("found county COVID data:", covidData);
-    //             return covidData;
-    //         }
-    //     })
-    //     .catch(function (err) {
-    //         console.log(err);
-    //         if (response) {
-    //             console.log(response.statusCode);
-    //         }
-    //         return false;
-    //     });
-
-
-
-// test covidReqHandler and validate location, REMOVE AFTER COMPLETING
-// let test_county_state = {county: "Orange", state: "CA"};
-// let some_data = covidReqHandler(test_county_state);
-// console.log("result: ", some_data);
-// (async () => {
-//     // let data = await getCovidData(test_county_state);
-//     console.log("got data back from API:", await getCovidData(test_county_state));
-// })();
+                // finished, send
+                res.render("results", {MAPBOX_KEY: process.env.MAPBOX_KEY, data: data} );
+            } else {
+                console.log("Error in finding the city/state.")
+                res.render("no-results");
+            }
+        });
+    }
+    else {
+        console.log('Error finding county.')
+        res.render("404");
+    }
+}
 
 app.get("/", function(req, res){
     var context = {};
@@ -251,47 +198,19 @@ app.get("/", function(req, res){
 app.get("/main-input-handler", function(req, res) {
     console.log("The user entered:", req.query.location);
 
-    // Find the county corresponding to the city and state
-    validateLocation(req.query.location, retrieve_covid_data);
-
-    function retrieve_covid_data(county_state_coords) {
-        console.log("county_state_coords in retrieve_covid_data():", county_state_coords);
-        // testing
-        // let county_state_coords = {county: "Orange", state: "CA"};
-        // let some_data = covidReqHandler(test_county_state);
-        // console.log("result: ", some_data);     // testing, remove when done
-        
-        if (county_state_coords["county"] !== "") {
-            // get the COVID data for that county
-            covidReqHandler(county_state_coords, function(data){
-                console.log("covid_data:", data);
-                res.render("results", {MAPBOX_KEY: process.env.MAPBOX_KEY, data: data} );
-            });
-        }
-    
-        // if (county_state_coords) {
-        //     // get the COVID data for that county
-        //     // let covid_data = covidReqHandler(county_state_coords);
-        //     // if (covid_data) {
-        //     //     res.render("results", covid_data);
-        //     // }
-        // } 
-        else {
-            res.render("no-results");
-        }
-    }
-
+    // acutal page rendering handled by retrieve_covid_data()
+    // find the county corresponding to the city and state
+    validateLocation(req.query.location, retrieve_covid_data, res);
 });
 
-app.get("/results", function(req, res){
-    // serve successful city search
-    // will the front-end handle request to api and handle api response?
-        // if so, then front end will request each page from server based on its response
-    var context = {};
-    res.status(200);
-    console.log(context);
-    res.render("results", {MAPBOX_KEY: process.env.MAPBOX_KEY});
-});
+// no need for /results route as that is handled my main-input-handler
+// app.get("/results", function(req, res){
+//     // serve successful city search
+//     var context = {};
+//     res.status(200);
+//     console.log(context);
+//     res.render("results", {MAPBOX_KEY: process.env.MAPBOX_KEY});
+// });
 
 app.get("/multiple-results", function(req, res){
     // user needs to select from multiple possible results
