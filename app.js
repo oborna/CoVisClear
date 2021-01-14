5c9ca00dab4b953e181f61bc3440acf6348baf8f
A O <aeodev@outlook.com>
2021-01-14 10:33:49 -0800

    Cleaned up code by removing old, unused code that was commented out.

--- app.js
+++ app.js
@@ -158,112 +158,6 @@ function covidReqHandler(county_state, callback){
     });
 }
 
-// const getCovidData = async (county_state) => {
-//     // generate the api request url based on county name
-//     let covidAPI = "https://www.trackcorona.live/api/cities/"
-//     let county_substrings = county_state.county.split(" ");
-//     for (let i=0; i < county_substrings.length; i++) {
-//         covidAPI += county_substrings[i];
-//         covidAPI += "%20";
-//     }
-//     covidAPI = covidAPI.slice(0, -3);        // remove the last %20
-
-//     const result = await covidRequest(covidAPI, county_state);
-//     return result;
-// }
-
-// const covidRequest = async (apiAddr, county_state) => {
-//     // send the request
-//     request(apiAddr, function(err, response, body) {
-//         if (!err && response.statusCode < 400) {
-//             let info = JSON.parse(response.body);
-//             let results = info.data;
-//             let covidData;
-
-//             if (results.length == 0) {
-//                 return false;
-//             } else if (results.length == 1){
-//                 covidData = results[0];
-//                 return covidData;
-//             } else {        // multiple county results, need to get the correct state
-//                 let state_to_find;
-//                 for (let j=0; j < states.length; j++) {
-//                     if (states[j].Code == county_state.state) {
-//                         state_to_find = states[j].State;
-//                     }
-//                 }
-//                 for (let i=0; i < results.length; i++) {
-//                     let state_cmp = results[i].location.split(",");
-//                     if (state_cmp[1].slice(1) == state_to_find) {
-//                         covidData = results[i];
-//                     }
-//                 }
-//                 console.log("found county COVID data:", covidData);
-//                 return covidData;
-//             }
-//         } else {
-//             if (response) {
-//                 console.log(response.statusCode);
-//             }
-//             // next(err);
-//         }
-//     });
-//     return response;
-// }
-
-    // // send the request
-    // let options = {
-    //     method: "GET",
-    //     uri: covidAPI
-    // }
-
-    // request_promise(options)
-    //     .then(function (response) {
-    //         let info = JSON.parse(response);
-    //         let results = info.data;
-    //         let covidData;
-
-    //         if (results.length == 0) {
-    //             return false;
-    //         } else if (results.length == 1){
-    //             covidData = results[0];
-    //             return covidData;
-    //         } else {        // multiple county results, need to get the correct state
-    //             let state_to_find;
-    //             for (let j=0; j < states.length; j++) {
-    //                 if (states[j].Code == county_state.state) {
-    //                     state_to_find = states[j].State;
-    //                 }
-    //             }
-    //             for (let i=0; i < results.length; i++) {
-    //                 let state_cmp = results[i].location.split(",");
-    //                 if (state_cmp[1].slice(1) == state_to_find) {
-    //                     covidData = results[i];
-    //                 }
-    //             }
-    //             console.log("found county COVID data:", covidData);
-    //             return covidData;
-    //         }
-    //     })
-    //     .catch(function (err) {
-    //         console.log(err);
-    //         if (response) {
-    //             console.log(response.statusCode);
-    //         }
-    //         return false;
-    //     });
-
-
-
-// test covidReqHandler and validate location, REMOVE AFTER COMPLETING
-// let test_county_state = {county: "Orange", state: "CA"};
-// let some_data = covidReqHandler(test_county_state);
-// console.log("result: ", some_data);
-// (async () => {
-//     // let data = await getCovidData(test_county_state);
-//     console.log("got data back from API:", await getCovidData(test_county_state));
-// })();
-
 app.get("/", function(req, res){
     var context = {};
     res.status(200);
@@ -281,7 +175,7 @@ app.get("/main-input-handler", function(req, res) {
 app.get("/results", function(req, res){
     // serve successful city search
     // will the front-end handle request to api and handle api response?
-        // if so, then front end will request each page from server based on its response
+    // if so, then front end will request each page from server based on its response
     var context = {};
     res.status(200);
     console.log(context);
